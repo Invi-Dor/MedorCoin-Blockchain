d76bdeecc2b813d626b9663ebff97da462ea1fe6
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 05:24:54 +0800

    Refactor addBlock to handle coinbase and UTXOs
    Refactor addBlock method to include coinbase transaction and UTXO management.

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,81 +1,59 @@
-#include <iostream>
-#include <ctime>
-#include "blockchain.h"
-#include "crypto.h"
+void Blockchain::addBlock(const std::string& minerAddress,
+                          std::vector<Transaction>& transactions) {
 
-using namespace std;
-
-Blockchain::Blockchain(string ownerAddr) {
-    ownerAddress = ownerAddr;
-    medor = 0x1e00ffff;
-    totalSupply = 0;
-    maxSupply = 50000000;
-
-    balances.clear();
-
-    // Add genesis
-    chain.push_back(createGenesisBlock());
-}
-
-Block Blockchain::createGenesisBlock() {
-    Block genesis("0", "Genesis Block", medor, ownerAddress);
-    genesis.timestamp = time(nullptr);
-
-    genesis.reward = 100;
-    totalSupply += genesis.reward;
-    balances[ownerAddress] += genesis.reward;
-
-    genesis.hash = doubleSHA256(genesis.headerToString());
-    return genesis;
-}
-
-uint64_t Blockchain::calculateReward() {
-    time_t now = time(nullptr);
-    time_t genesisTime = chain[0].timestamp;
-    double months = difftime(now, genesisTime) / (60*60*24*30.0);
-
-    if (months <= 2.0) return 55;
-    return 30;
-}
-
-void Blockchain::mineBlock(Block &block) {
-    string target = "0";
-    do {
-        block.nonce++;
-        block.hash = doubleSHA256(block.headerToString());
-    } while (block.hash.substr(0, target.size()) != target);
-}
-
-void Blockchain::addBlock(const string &data, const string &minerAddr) {
+    // 1. Calculate block reward
     uint64_t reward = calculateReward();
-    if (totalSupply + reward > maxSupply)
+    if (totalSupply + reward > maxSupply) {
         reward = maxSupply - totalSupply;
+    }
 
-    Block newBlock(chain.back().hash, data, medor, minerAddr);
-    newBlock.timestamp = time(nullptr);
-    newBlock.reward = reward;
-
-    mineBlock(newBlock);
-    chain.push_back(newBlock);
+    // 2. Create coinbase transaction (block reward)
+    Transaction coinbaseTx;
+    TxOutput minerOut;
+    TxOutput ownerOut;
 
     uint64_t minerReward = (reward * 90) / 100;
     uint64_t ownerReward = reward - minerReward;
 
-    balances[minerAddr] += minerReward;
-    balances[ownerAddress] += ownerReward;
-    totalSupply += reward;
-}
+    minerOut.value = minerReward;
+    minerOut.address = minerAddress;
 
-void Blockchain::printChain() const {
-    for (size_t i = 0; i < chain.size(); i++) {
-        cout << "Block " << i << ":\n";
-        cout << "  PrevHash: " << chain[i].previousHash << "\n";
-        cout << "  Hash: " << chain[i].hash << "\n";
-        cout << "  Reward: " << chain[i].reward << "\n";
-        cout << "  Miner: " << chain[i].minerAddress << "\n\n";
+    ownerOut.value = ownerReward;
+    ownerOut.address = ownerAddress;
+
+    coinbaseTx.outputs.push_back(minerOut);
+    coinbaseTx.outputs.push_back(ownerOut);
+    coinbaseTx.calculateHash();
+
+    // 3. Insert coinbase transaction at the start
+    transactions.insert(transactions.begin(), coinbaseTx);
+
+    // 4. Create new block
+    Block newBlock(chain.back().hash, "MedorCoin Block", medor, minerAddress);
+    newBlock.timestamp = time(nullptr);
+    newBlock.reward = reward;
+    newBlock.transactions = transactions;
+
+    // 5. Mine the block (lightweight PoW)
+    mineBlock(newBlock);
+
+    // 6. Add block to chain
+    chain.push_back(newBlock);
+
+    // 7. Update UTXO set (THIS IS THE PART YOU ASKED ABOUT)
+    for (auto& tx : newBlock.transactions) {
+
+        // Spend inputs
+        for (auto& in : tx.inputs) {
+            utxoSet.spendUTXO(in.prevTxHash, in.outputIndex);
+        }
+
+        // Create new UTXOs from outputs
+        for (size_t i = 0; i < tx.outputs.size(); ++i) {
+            utxoSet.addUTXO(tx.outputs[i], tx.txHash, static_cast<int>(i));
+        }
     }
-    cout << "=== Balances ===\n";
-    for (auto &p : balances)
-        cout << p.first << ": " << p.second << " MEDOR\n";
-    cout << "Total: " << totalSupply << " MEDOR\n";
+
+    // 8. Update total supply
+    totalSupply += reward;
 }
