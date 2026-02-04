8dedb37f31f53fe4ada76cabf88adfae06b10bcf
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 05:02:07 +0800

    Refactor UTXO structure and update methods

--- src/utxo.h
+++ src/utxo.h
@@ -1,30 +1,20 @@
-#ifndef UTXO_H
-#define UTXO_H
-
+#pragma once
+#include <map>
 #include <string>
-#include <vector>
-#include <cstdint>
 #include "transaction.h"
 
 struct UTXO {
-    std::string txid;
-    uint32_t index;
+    std::string txHash;
+    int index;
     uint64_t value;
-    std::string owner;
+    std::string address;
 };
 
 class UTXOSet {
 public:
-    static UTXOSet& getInstance();
+    std::map<std::string, UTXO> utxos; // key = txHash:index
 
-    void addUTXO(const UTXO& utxo);
-    void removeUTXO(const std::string& txid, uint32_t index);
-    std::vector<UTXO> getUTXOs(const std::string& address);
-
-private:
-    std::vector<UTXO> utxos;
-
-    UTXOSet() = default;
+    void addUTXO(const TxOutput &output, const std::string &txHash, int index);
+    void spendUTXO(const std::string &txHash, int index);
+    uint64_t getBalance(const std::string &address);
 };
-
-#endif
