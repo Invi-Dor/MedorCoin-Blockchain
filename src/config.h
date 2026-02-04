81e509b53613fb6e6f32035f9b472ff6f93e0102
Ajvlogger <ajvlogger4@gmail.com>
2026-02-04 13:45:12 +0800

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+
+const std::string COIN_NAME = "MedorCoin";
+const std::string COIN_SYMBOL = "MEDOR";
+const uint32_t INITIAL_MEDOR = 0x1e00ffff; // Difficulty
+
+#endif
