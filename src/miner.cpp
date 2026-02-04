80989b2021eb318575d1f4e3e9d275cbfb37b862
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 04:50:01 +0800

    Refactor mining logic and add MedorCoin mining

--- src/miner.cpp
+++ src/miner.cpp
@@ -1,25 +1,6 @@
 #include "miner.h"
-#include <iostream>
+#include "blockchain.h"
 
-Miner::Miner(Blockchain& bc) : chain(bc) {}
-
-void Miner::minePendingTransactions() {
-    auto transactions = Mempool::getInstance().getTransactions();
-    if (transactions.empty()) {
-        std::cout << "No transactions to mine." << std::endl;
-        return;
-    }
-
-    MerkleTree tree(transactions);
-    std::string merkleRoot = tree.root;
-
-    std::string txData;
-    for (auto &tx : transactions) txData += tx.txid;
-
-    Block block(chain.chain.back().hash, txData, chain.medor);
-    chain.chain.push_back(block);
-
-    Mempool::getInstance().clear();
-
-    std::cout << "Block mined with Merkle root: " << merkleRoot << std::endl;
+void Miner::mineMedor(Blockchain &chain, const std::string &minerAddress) {
+    chain.addBlock("Mined MedorCoin block", minerAddress);
 }
