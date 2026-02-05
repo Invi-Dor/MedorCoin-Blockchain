e2db2e13dc61cb930fe60435fee182394144d620
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 10:13:10 +0800

    Refactor Blockchain class and update methods

--- src/blockchain.h
+++ src/blockchain.h
@@ -1,25 +1,23 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <map>
+#include <cstdint>
 #include "block.h"
+#include "transaction.h"
+#include "utxo.h"
 
 class Blockchain {
 public:
     std::vector<Block> chain;
+    UTXOSet utxoSet;
+    std::string ownerAddress;
     uint32_t medor;
-
     uint64_t totalSupply;
     uint64_t maxSupply;
 
-    std::string ownerAddress;
-    std::map<std::string, uint64_t> balances;
+    Blockchain(const std::string& ownerAddr);
 
-    Blockchain(std::string ownerAddr);
-
-    Block createGenesisBlock();
     uint64_t calculateReward();
-    void mineBlock(Block &block);
-    void addBlock(const std::string &data, const std::string &minerAddr);
-    void printChain() const;
+    void mineBlock(Block& block);
+    void addBlock(const std::string& minerAddress, std::vector<Transaction>& transactions);
 };
