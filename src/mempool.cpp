1f0d60a10ac9e1f0e3727177aa0208dc5f102781
Ajvlogger <ajvlogger4@gmail.com>
2026-02-06 06:40:18 +0800

    Update mempool.cpp

--- src/mempool.cpp
+++ src/mempool.cpp
@@ -1,34 +1,37 @@
 #include "mempool.h"
 
-// Add a transaction to the mempool if it doesn’t already exist
+// Add transaction to mempool if not already present
 bool Mempool::addTransaction(const Transaction &tx) {
     std::lock_guard<std::mutex> lock(mtx);
-    // If transaction hash already exists, don’t add
-    if (txMap.find(tx.txHash) != txMap.end()) {
+
+    const std::string &hash = tx.getHash();
+    if (txMap.find(hash) != txMap.end()) {
         return false;
     }
-    txMap[tx.txHash] = tx;
+
+    txMap[hash] = tx;
     return true;
 }
 
-// Remove a transaction by its hash
+// Remove a transaction using its hash
 void Mempool::removeTransaction(const std::string &txHash) {
     std::lock_guard<std::mutex> lock(mtx);
     txMap.erase(txHash);
 }
 
-// Get all transactions currently in the mempool
+// Return a vector of all transactions in the mempool
 std::vector<Transaction> Mempool::getTransactions() {
     std::lock_guard<std::mutex> lock(mtx);
     std::vector<Transaction> txs;
     txs.reserve(txMap.size());
+
     for (auto &kv : txMap) {
         txs.push_back(kv.second);
     }
     return txs;
 }
 
-// Check if a transaction exists in the mempool
+// Check if a transaction exists by its hash
 bool Mempool::hasTransaction(const std::string &txHash) {
     std::lock_guard<std::mutex> lock(mtx);
     return txMap.find(txHash) != txMap.end();
