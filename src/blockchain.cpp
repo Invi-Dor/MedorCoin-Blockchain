8514fb1e23510b55efd28137cae6ee6b443bccf4
Ajvlogger <ajvlogger4@gmail.com>
2026-02-05 09:36:55 +0800

    Refactor addBlock method and improve readability

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -1,16 +1,50 @@
+#include "blockchain.h"
+#include "block.h"
+#include "transaction.h"
+#include "utxo.h"
+#include "consensus.h"
+#include <ctime>      // for time()
+#include <cstdint>    // for uint32_t, uint64_t
+#include <vector>
+#include <string>
+
+// Constructor definition
+Blockchain::Blockchain(const std::string& ownerAddr) {
+    ownerAddress = ownerAddr;
+    medor = 0x1e00ffff;
+    totalSupply = 0;
+    maxSupply = 50000000;
+}
+
+// Calculate reward
+uint64_t Blockchain::calculateReward() {
+    time_t now = time(nullptr);
+    time_t genesisTime = chain.empty() ? now : chain[0].timestamp;
+    double months = difftime(now, genesisTime) / (60 * 60 * 24 * 30.0);
+    if (months <= 2.0) return 55;
+    return 30;
+}
+
+// Lightweight mining
+void Blockchain::mineBlock(Block& block) {
+    std::string target = "0";
+    do {
+        block.nonce++;
+        block.hash = doubleSHA256(block.headerToString());
+    } while (block.hash.substr(0, target.size()) != target);
+}
+
+// Add block
 void Blockchain::addBlock(const std::string& minerAddress,
                           std::vector<Transaction>& transactions) {
 
-    // 1. Calculate block reward
     uint64_t reward = calculateReward();
     if (totalSupply + reward > maxSupply) {
         reward = maxSupply - totalSupply;
     }
 
-    // 2. Create coinbase transaction (block reward)
     Transaction coinbaseTx;
-    TxOutput minerOut;
-    TxOutput ownerOut;
+    TxOutput minerOut, ownerOut;
 
     uint64_t minerReward = (reward * 90) / 100;
     uint64_t ownerReward = reward - minerReward;
@@ -25,35 +59,27 @@ void Blockchain::addBlock(const std::string& minerAddress,
     coinbaseTx.outputs.push_back(ownerOut);
     coinbaseTx.calculateHash();
 
-    // 3. Insert coinbase transaction at the start
     transactions.insert(transactions.begin(), coinbaseTx);
 
-    // 4. Create new block
-    Block newBlock(chain.back().hash, "MedorCoin Block", medor, minerAddress);
+    Block newBlock(chain.empty() ? Block("", "", medor, minerAddress)
+                                 : Block(chain.back().hash, "MedorCoin Block", medor, minerAddress);
+
     newBlock.timestamp = time(nullptr);
     newBlock.reward = reward;
     newBlock.transactions = transactions;
 
-    // 5. Mine the block (lightweight PoW)
     mineBlock(newBlock);
 
-    // 6. Add block to chain
     chain.push_back(newBlock);
 
-    // 7. Update UTXO set (THIS IS THE PART YOU ASKED ABOUT)
     for (auto& tx : newBlock.transactions) {
-
-        // Spend inputs
         for (auto& in : tx.inputs) {
             utxoSet.spendUTXO(in.prevTxHash, in.outputIndex);
         }
-
-        // Create new UTXOs from outputs
         for (size_t i = 0; i < tx.outputs.size(); ++i) {
             utxoSet.addUTXO(tx.outputs[i], tx.txHash, static_cast<int>(i));
         }
     }
 
-    // 8. Update total supply
     totalSupply += reward;
 }
