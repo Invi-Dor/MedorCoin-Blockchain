c2163cc4927409669a6fe1c4113e87c12358e18b
Ajvlogger <ajvlogger4@gmail.com>
2026-02-11 02:18:36 +0800

    Update execute.h

--- src/evm/execute.h
+++ src/evm/execute.h
@@ -2,17 +2,23 @@
 #define MEDOR_EVM_EXECUTE_H
 
 #include <vector>
-#include <string>
+#include <cstdint>
 #include <evmc/evmc.hpp>
 
+class EVMStorage;
+
+// Executes EVM code using persistent storage
 class EVMExecutor
 {
 public:
-    static evmc_result executeContract(const std::vector<uint8_t>& bytecode,
-                                       const std::vector<uint8_t>& inputData,
-                                       uint64_t gasLimit,
-                                       evmc_address to,
-                                       evmc_address from);
+    static evmc_result executeContract(
+        EVMStorage &storage,
+        const std::vector<uint8_t> &bytecode,
+        const std::vector<uint8_t> &inputData,
+        uint64_t gasLimit,
+        evmc_address to,
+        evmc_address from
+    );
 };
 
 #endif // MEDOR_EVM_EXECUTE_H
