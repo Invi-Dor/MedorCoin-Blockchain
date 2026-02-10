db5ef97726197137f1b7436cba65d7a05f9004bf
Ajvlogger <ajvlogger4@gmail.com>
2026-02-11 03:28:19 +0800

    Update miner.h

--- src/miner.h
+++ src/miner.h
@@ -1,8 +1,14 @@
 #pragma once
+
 #include <string>
 #include "blockchain.h"
 
 class Miner {
 public:
+    // Mine a MedorCoin block
     void mineMedor(Blockchain &chain, const std::string &minerAddress);
+
+    // Optionally include mempool transactions
+    void mineWithMempool(Blockchain &chain,
+                         const std::string &minerAddress);
 };
