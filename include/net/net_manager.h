1bec9e225a88051ad168b90b981bcbff27983b6b
Ajvlogger <ajvlogger4@gmail.com>
2026-02-13 23:23:05 +0800

    Update net_manager.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- include/net/net_manager.h
+++ include/net/net_manager.h
@@ -1,46 +1,44 @@
 #pragma once
-
 #ifndef NET_MANAGER_H
 #define NET_MANAGER_H
 
 #include <string>
 #include <vector>
 #include <functional>
-#include <memory>
 #include <nlohmann/json.hpp>
 
-// Forward declare PeerManager to avoid including its header here
-class PeerManager;
-
 namespace libp2p {
-    namespace host {
-        class Host;
-    }
+    namespace host { class Host; }
+    namespace network { class Stream; class PeerId; }
 }
 
+// Manages libp2p peer connections and message handlers
 class NetworkManager {
 public:
     explicit NetworkManager(const std::string &listenAddr);
     ~NetworkManager();
 
-    // Start listening for incoming connections
+    // Start listening on libp2p swarm
     bool start();
 
-    // Connect to a list of bootstrap peers
+    // Connect to bootstrap multiaddresses
     bool connectBootstrap(const std::vector<std::string> &bootstrap);
 
-    // Broadcast a signed block message (JSON) over P2P
+    // Send a JSON block message to peers
     void broadcastBlock(const nlohmann::json &blockMsg);
 
-    // Broadcast a transaction message (JSON)
+    // Send a JSON tx message to peers
     void broadcastTx(const nlohmann::json &txMsg);
 
-    // Register a message handler callback for incoming JSON messages
+    // Register a callback to handle incoming messages
     void onMessage(const std::function<void(const nlohmann::json &)> &handler);
 
 private:
-    PeerManager* peerMgr;  // abstract peer manager pointer
-    std::shared_ptr<libp2p::host::Host> hostPtr;  // optional libp2p host
+    std::shared_ptr<libp2p::host::Host> host;
+    std::function<void(const nlohmann::json &)> messageHandler;
+
+    void initProtocols();
+    void handleStream(std::shared_ptr<libp2p::network::Stream> stream);
 };
 
 #endif // NET_MANAGER_H
