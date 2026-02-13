2df14b88728a53497203e84e6dc17931ed7499a2
Ajvlogger <ajvlogger4@gmail.com>
2026-02-13 22:33:53 +0800

    Update blockchain.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/blockchain.h
+++ src/blockchain.h
@@ -3,12 +3,14 @@
 #include <vector>
 #include <string>
 #include <cstdint>
-#include <array>
 #include "block.h"
 #include "transaction.h"
 #include "utxo.h"
 #include "db/blockdb.h"
 #include "db/accountdb.h"
+
+// Forward
+namespace consensus { class ValidatorRegistry; }
 
 class Blockchain {
 public:
@@ -34,23 +36,21 @@ public:
     void setBalance(const std::string &addr, uint64_t amount);
     void addBalance(const std::string &addr, uint64_t amount);
 
-    uint64_t getBalance(const std::array<uint8_t,20> &addr) const;
-    void setBalance(const std::array<uint8_t,20> &addr, uint64_t amount);
-    void addBalance(const std::array<uint8_t,20> &addr, uint64_t amount);
-
-    bool verifyTransactionSignature(const Transaction &tx) const;
-    bool executeTransaction(const Transaction &tx, const std::string &minerAddress);
-    std::vector<Transaction> getTransactions(const std::vector<std::string> &hashes) const;
-
-    uint64_t calculateReward();
-    void mineBlock(Block &block);
+    uint64_t getNonce(const std::string &addr) const;
+    bool findTransaction(const std::string &hash, Transaction &txOut) const;
 
     void addBlock(const std::string &minerAddress,
                   std::vector<Transaction> &transactions);
 
     bool validateBlock(const Block &block, const Block &previousBlock);
     bool validateChain();
+
     void printChain() const;
+
+    // ------------------------
+    // PoA Validator check
+    // ------------------------
+    bool isValidator(const std::string &addrHex) const;
 
 private:
     uint64_t baseFeePerGas = 1;
