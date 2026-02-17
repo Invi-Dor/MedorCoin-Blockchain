44eb811cfe450113984b40647f645f1288b06efa
Ajvlogger <ajvlogger4@gmail.com>
2026-02-17 23:46:45 +0800

    Update keccak.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/keccak/keccak.h
+++ src/crypto/keccak/keccak.h
@@ -1,9 +1,1 @@
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
 
-// outlen in bytes (32 for Keccak‑256)
-void keccak(const uint8_t *in, size_t inlen, uint8_t *out, size_t outlen);
-
-// Internal f‑function
-void keccakf(uint64_t st[25]);
