f2d24ec1856f88525f3cd197fc614f065b78e8b5
Ajvlogger <ajvlogger4@gmail.com>
2026-02-23 01:15:06 +0800

    Update api.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- include/api.h
+++ include/api.h
@@ -1,14 +1,23 @@
 #ifndef MEDOR_API_H
 #define MEDOR_API_H
 
-void startAPIServer();
-
-#endif
-
-// API.h
-#pragma once
 #include <string>
 #include <vector>
 #include "utxo.h"
+#include "transaction.h"
 
+// Starts the HTTP API server
+void startAPIServer();
+
+// UTXO endpoints
 std::vector<UTXO> getUTXOs(const std::string& address);
+
+// Transaction endpoints
+Transaction createTransaction(const std::string& from, const std::string& to, uint64_t amount, uint64_t fee);
+Transaction signTransaction(const Transaction& tx, const std::string& privKeyHex);
+bool broadcastTransaction(const Transaction& tx);
+
+// History
+std::vector<Transaction> getTransactionHistory(const std::string& address);
+
+#endif // MEDOR_API_H
