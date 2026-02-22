3636d4f84394ef0d75c2a1e999fe0e8f3d67e78d
Ajvlogger <ajvlogger4@gmail.com>
2026-02-23 00:22:06 +0800

    Update api.cpp
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/api.cpp
+++ src/api.cpp
@@ -3,3 +3,18 @@ POST /api/tx/create
 POST /api/tx/sign
 POST /api/tx/broadcast
 GET /api/tx/history
+
+// API.cpp
+#include "api.h"
+#include "blockchain.h"
+
+// GET /api/tx/history?address={address}
+std::vector<Transaction> getTransactionHistory(const std::string& address) {
+    std::vector<Transaction> history;
+    for (const auto& tx : blockchain.transactions) {
+        if (tx.hasAddress(address)) {
+            history.push_back(tx);
+        }
+    }
+    return history;
+}
