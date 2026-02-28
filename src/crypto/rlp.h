45e0c6c503f8135ed8db49c710f437d57e06d3ca
Ajvlogger <ajvlogger4@gmail.com>
2026-03-01 07:19:05 +0800

    Update rlp.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/rlp.h
+++ src/crypto/rlp.h
@@ -2,9 +2,19 @@
 #define RLP_H
 
 #include <vector>
-#include <cstddef>
 #include <cstdint>
 
-std::vector<unsigned char> rlp_encode(const std::vector<unsigned char>& input);
+/**
+ * Recursive Length Prefix (RLP) encode functions.
+ * This matches the Ethereum RLP specification:
+ * strings and lists encoded according to length and payload rules.  [oai_citation:0‡ethereum.org](https://ethereum.org/developers/docs/data-structures-and-encoding/rlp/?utm_source=chatgpt.com)
+ */
+namespace rlp {
+
+std::vector<uint8_t> encodeBytes(const std::vector<uint8_t>& input);
+std::vector<uint8_t> encodeUInt(uint64_t value);
+std::vector<uint8_t> encodeList(const std::vector<std::vector<uint8_t>>& items);
+
+} // namespace rlp
 
 #endif // RLP_H
