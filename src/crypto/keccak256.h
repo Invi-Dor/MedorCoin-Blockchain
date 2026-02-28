036f8e47d0e09b40443bfb19ae9b00d2f2cab5f5
Ajvlogger <ajvlogger4@gmail.com>
2026-03-01 07:12:32 +0800

    Update keccak256.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/keccak256.h
+++ src/crypto/keccak256.h
@@ -1,27 +1,13 @@
 #ifndef KECCAK256_H
 #define KECCAK256_H
 
-#include <cstdint>
-#include <array>
 #include <cstddef>
+#include <cstdint>
+#include <vector>
+#include <array>
 
 namespace crypto {
-
-class Keccak256 {
-public:
-    static constexpr size_t HASH_SIZE = 32;
-    Keccak256() noexcept;
-    void update(const uint8_t* data, size_t length) noexcept;
-    std::array<uint8_t, HASH_SIZE> digest() noexcept;
-    void reset() noexcept;
-
-private:
-    void keccakf1600(std::array<uint8_t, 200>& state) noexcept;
-    std::array<uint8_t, 200> state_;
-    size_t rate_;
-    size_t pos_;
-};
-
-} // namespace crypto
+    std::vector<unsigned char> Keccak256(const std::vector<unsigned char>& data);
+}
 
 #endif // KECCAK256_H
