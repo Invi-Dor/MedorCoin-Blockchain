227d452c5b9ed75b09ee89e130750cdc35709788
Ajvlogger <ajvlogger4@gmail.com>
2026-03-01 07:15:21 +0800

    Update keccak256.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/keccak256.h
+++ src/crypto/keccak256.h
@@ -1,10 +1,9 @@
 #ifndef KECCAK256_H
 #define KECCAK256_H
 
+#include <vector>
 #include <cstddef>
 #include <cstdint>
-#include <vector>
-#include <array>
 
 namespace crypto {
     std::vector<unsigned char> Keccak256(const std::vector<unsigned char>& data);
