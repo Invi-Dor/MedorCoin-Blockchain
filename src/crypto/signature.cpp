f953c4c0b9f09c1c585e52e99b25068f6e97e705
Ajvlogger <ajvlogger4@gmail.com>
2026-03-01 07:11:39 +0800

    Update signature.cpp
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/signature.cpp
+++ src/crypto/signature.cpp
@@ -10,14 +10,17 @@ bool sign_hash(
 ) {
     if (!hash32 || !privkey32 || !out_sig65) return false;
 
-    secp256k1_context* ctx =
-        secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
     if (!ctx) return false;
 
     secp256k1_ecdsa_recoverable_signature recsig;
     if (secp256k1_ecdsa_sign_recoverable(
-            ctx, &recsig, hash32, privkey32,
-            secp256k1_nonce_function_default, NULL) != 1) {
+            ctx,
+            &recsig,
+            hash32,
+            privkey32,
+            secp256k1_nonce_function_default,
+            NULL) != 1) {
         secp256k1_context_destroy(ctx);
         return false;
     }
@@ -45,13 +48,11 @@ bool verify_hash(
 ) {
     if (!hash32 || !sig65 || !pubkey_bytes) return false;
 
-    secp256k1_context* ctx =
-        secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
     if (!ctx) return false;
 
     secp256k1_pubkey pubkey;
-    if (secp256k1_ec_pubkey_parse(ctx, &pubkey,
-                                  pubkey_bytes, pubkey_len) != 1) {
+    if (secp256k1_ec_pubkey_parse(ctx, &pubkey, pubkey_bytes, pubkey_len) != 1) {
         secp256k1_context_destroy(ctx);
         return false;
     }
@@ -64,12 +65,9 @@ bool verify_hash(
     }
 
     secp256k1_ecdsa_signature normsig;
-    secp256k1_ecdsa_recoverable_signature_convert(
-        ctx, &normsig, &recsig);
+    secp256k1_ecdsa_recoverable_signature_convert(ctx, &normsig, &recsig);
 
-    int ok = secp256k1_ecdsa_verify(
-        ctx, &normsig, hash32, &pubkey);
-
+    int ok = secp256k1_ecdsa_verify(ctx, &normsig, hash32, &pubkey);
     secp256k1_context_destroy(ctx);
     return ok == 1;
 }
@@ -81,31 +79,26 @@ bool recover_pubkey(
 ) {
     if (!hash32 || !sig65 || !out_pubkey) return false;
 
-    int recid = (int)sig65[64];
-    if (recid < 0 || recid > 3) return false;
-
-    secp256k1_context* ctx =
-        secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
     if (!ctx) return false;
 
     secp256k1_ecdsa_recoverable_signature recsig;
     if (secp256k1_ecdsa_recoverable_signature_parse_compact(
-            ctx, &recsig, sig65, recid) != 1) {
+            ctx, &recsig, sig65, (int)sig65[64]) != 1) {
         secp256k1_context_destroy(ctx);
         return false;
     }
 
     secp256k1_pubkey pubkey;
-    if (secp256k1_ecdsa_recover(
-            ctx, &pubkey, &recsig, hash32) != 1) {
+    if (secp256k1_ecdsa_recover(ctx, &pubkey, &recsig, hash32) != 1) {
         secp256k1_context_destroy(ctx);
         return false;
     }
 
     size_t outlen = 33;
     if (secp256k1_ec_pubkey_serialize(
-            ctx, out_pubkey, &outlen,
-            &pubkey, SECP256K1_EC_COMPRESSED) != 1) {
+            ctx, out_pubkey, &outlen, &pubkey,
+            SECP256K1_EC_COMPRESSED) != 1) {
         secp256k1_context_destroy(ctx);
         return false;
     }
