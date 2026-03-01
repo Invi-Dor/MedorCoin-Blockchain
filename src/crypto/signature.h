34beacd56c25e91a15ccf187bf23000f07c7187e
Ajvlogger <ajvlogger4@gmail.com>
2026-03-01 12:15:49 +0800

    Update signature.h
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- src/crypto/signature.h
+++ src/crypto/signature.h
@@ -1,35 +1,13 @@
-#ifndef SIGNATURE_H
-#define SIGNATURE_H
-
-#include <array>
-#include <tuple>
-#include <cstdint>
+include <array>
+include <tuple>
 
 namespace crypto {
 
-std::tuple<
-    std::array<uint8_t,32>,
-    std::array<uint8_t,32>,
-    uint8_t
-> signHash(
-    const std::array<uint8_t,32>& digest,
-    const std::array<uint8_t,32>& privKeyBytes
-);
-
-bool verifyHash(
-    const std::array<uint8_t,32>& digest,
-    const std::array<uint8_t,32>& sig64,
-    uint8_t recid,
-    const std::array<uint8_t,33>& pubkeyBytes
-);
-
-bool recoverPubkey(
-    const std::array<uint8_t,32>& digest,
-    const std::array<uint8_t,32>& sig64,
-    uint8_t recid,
-    std::array<uint8_t,33>& outPubkey
-);
+// Returns (r, s, v) where r and s are 32-byte values and v is the recovery id (0..3)
+std::tuple<std::array<uint8_t, 32>, std::array<uint8_t, 32>, uint8_t> signHash(
+    const std::array<uint8_t, 32>& digest,
+    const std::array<uint8_t, 32>& privkey);
 
 } // namespace crypto
 
-#endif // SIGNATURE_H
+endif // SIGNATURE_H
