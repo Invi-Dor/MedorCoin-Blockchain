138372513847ae0ed665011e61c30a45a5b866b1
Ajvlogger <ajvlogger4@gmail.com>
2026-03-09 14:41:33 +0800

    Update build.sh
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Compiling Medorcoin backend..."
-g++ \
-  -I./src/crypto \
-  -I./src/abi \
-  -I/usr/include \
-  -I/usr/local/include \
-  ./src/crypto/signature.cpp \
-  ./src/crypto/verify_signature.cpp \
-  ./src/crypto/evm_sign.cpp \
-  ./src/crypto/keccak256.cpp \
-  ./src/crypto/rlp.cpp \
-  ./src/abi/encode.cpp \
-  -L/usr/lib/x86_64-linux-gnu \
-  -lsecp256k1 \
-  -o medorcoin_backend
+OUTPUT="medorcoin"
+LOGFILE="compile_errors.log"
 
-echo "Build succeeded."
+echo "Compiling MedorCoin backend..."
+
+# Include directories
+INCLUDES="-I./include -I./src -I./src/crypto -I./src/abi -I/usr/include -I/usr/local/include"
+
+# Find all .cpp files
+SRC_FILES=$(find src -name "*.cpp")
+ROOT_FILES=$(find . -maxdepth 1 -name "*.cpp")
+
+# Compile with secp256k1 library
+g++ $SRC_FILES $ROOT_FILES $INCLUDES -std=c++20 -pthread -O2 -L/usr/lib/x86_64-linux-gnu -lsecp256k1 -o $OUTPUT 2> $LOGFILE
+
+# Check compilation
+if [ $? -eq 0 ]; then
+    echo "Compilation successful! Binary created: $OUTPUT"
+    rm -f $LOGFILE
+else
+    echo "Compilation failed. Check $LOGFILE for details."
+fi
