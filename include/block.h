0ddd0f76cd1995a6d1cac97de0497e9d66cc5b94
food101ideas <food101.ideas@gmail.com>
2026-03-19 07:12:56 +0800

    Update block.h
    Signed-off-by: food101ideas <food101.ideas@gmail.com>

--- include/block.h
+++ include/block.h
@@ -3,26 +3,32 @@
 #include <string>
 #include <vector>
 #include <ctime>
-
+#include <cstdint>
 #include "transaction.h"
 
 class Block {
 public:
-    std::string previousHash;
-    std::string data;
-    unsigned int difficulty;
-    std::string minerAddress;
-    time_t timestamp;
-    unsigned long nonce;
-    unsigned long reward;
-    std::string hash;
+    std::string              previousHash;
+    std::string              data;
+    unsigned int             difficulty  = 0;
+    std::string              minerAddress;
+    time_t                   timestamp   = 0;
+    unsigned long            nonce       = 0;
+    uint64_t                 reward      = 0;
+    uint64_t                 baseFee     = 0;
+    uint64_t                 gasUsed     = 0;
+    std::string              hash;
+    std::string              signature;
     std::vector<Transaction> transactions;
 
     Block();
     Block(const std::string& prevHash,
           const std::string& blockData,
-          unsigned int diff,
+          unsigned int       diff,
           const std::string& minerAddr);
 
-    std::string headerToString() const;
+    std::string headerToString()              const;
+    std::string serialize()                   const;
+    void        deserialize(const std::string& data);
+    std::vector<uint8_t> serializeHeader()    const;
 };
