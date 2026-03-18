a3ac25040025c55ac8e70482a1589c20e28b1283
food101ideas <food101.ideas@gmail.com>
2026-03-19 07:16:55 +0800

    Update block.cpp
    Signed-off-by: food101ideas <food101.ideas@gmail.com>

--- src/block.cpp
+++ src/block.cpp
@@ -1,10 +1,13 @@
 #include "block.h"
+#include "crypto/keccak256.h"
 #include <sstream>
+#include <iomanip>
 
 Block::Block()
     : previousHash(""), data(""), difficulty(0),
       minerAddress(""), timestamp(time(nullptr)),
-      nonce(0), reward(0), hash("") {}
+      nonce(0), reward(0), baseFee(0), gasUsed(0),
+      hash(""), signature("") {}
 
 Block::Block(const std::string& prevHash,
              const std::string& blockData,
@@ -17,10 +20,72 @@ Block::Block(const std::string& prevHash,
       timestamp(time(nullptr)),
       nonce(0),
       reward(0),
-      hash("") {}
+      baseFee(0),
+      gasUsed(0),
+      hash(""),
+      signature("") {}
 
 std::string Block::headerToString() const {
-    std::stringstream ss;
-    ss << previousHash << timestamp << nonce << difficulty << minerAddress;
+    std::ostringstream ss;
+    ss << previousHash << timestamp << nonce
+       << difficulty << minerAddress
+       << baseFee << gasUsed;
     return ss.str();
+}
+
+std::vector<uint8_t> Block::serializeHeader() const {
+    std::string h = headerToString();
+    return std::vector<uint8_t>(h.begin(), h.end());
+}
+
+std::string Block::serialize() const {
+    std::ostringstream ss;
+    ss << previousHash << "|"
+       << data         << "|"
+       << difficulty   << "|"
+       << minerAddress << "|"
+       << timestamp    << "|"
+       << nonce        << "|"
+       << reward       << "|"
+       << baseFee      << "|"
+       << gasUsed      << "|"
+       << hash         << "|"
+       << signature    << "|"
+       << transactions.size();
+    for (const auto& tx : transactions)
+        ss << "|" << tx.txHash
+           << "|" << tx.toAddress
+           << "|" << tx.value
+           << "|" << tx.nonce;
+    return ss.str();
+}
+
+void Block::deserialize(const std::string& raw) {
+    std::istringstream ss(raw);
+    std::string token;
+    auto next = [&]() -> std::string {
+        std::getline(ss, token, '|');
+        return token;
+    };
+    previousHash = next();
+    data         = next();
+    difficulty   = static_cast<unsigned int>(std::stoul(next()));
+    minerAddress = next();
+    timestamp    = static_cast<time_t>(std::stoll(next()));
+    nonce        = std::stoul(next());
+    reward       = std::stoull(next());
+    baseFee      = std::stoull(next());
+    gasUsed      = std::stoull(next());
+    hash         = next();
+    signature    = next();
+    size_t txCount = std::stoull(next());
+    transactions.clear();
+    for (size_t i = 0; i < txCount; i++) {
+        Transaction tx;
+        tx.txHash    = next();
+        tx.toAddress = next();
+        tx.value     = std::stoull(next());
+        tx.nonce     = std::stoull(next());
+        transactions.push_back(tx);
+    }
 }
