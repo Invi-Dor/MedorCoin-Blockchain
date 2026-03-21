2b37a1729c9c51eee946295892d87e2332b734cc
food101ideas <food101.ideas@gmail.com>
2026-03-21 11:18:33 +0800

    Update evm_sign.cpp
    Signed-off-by: food101ideas <food101.ideas@gmail.com>

--- src/crypto/evm_sign.cpp
+++ src/crypto/evm_sign.cpp
@@ -1,42 +1,42 @@
-#include "evm_sign.h"
-#include "rlp.h"
-#include "keccak256.h"
-#include "signature.h"
+#include "crypto/evm_sign.h"
+#include "crypto/rlp.h"
+#include "crypto/keccak256.h"
+#include "crypto/signature.h"
 
-#include <vector>
 #include <array>
 #include <cstring>
+#include <vector>
 
 namespace crypto {
 
-static std::vector<uint8_t> buildUnsignedRlp(const EvmTx &tx) {
+static std::vector<uint8_t> buildUnsignedRlp(const EvmTx &tx)
+{
     using namespace rlp;
-
     std::vector<std::vector<uint8_t>> fields;
-    fields.push_back( encodeUInt(tx.chainId) );
-    fields.push_back( encodeUInt(tx.nonce) );
-    fields.push_back( encodeUInt(tx.maxPriorityFeePerGas) );
-    fields.push_back( encodeUInt(tx.maxFeePerGas) );
-    fields.push_back( encodeUInt(tx.gasLimit) );
-    fields.push_back( encodeBytes(tx.to) );
-    fields.push_back( encodeBytes(tx.value) );
-    fields.push_back( encodeBytes(tx.data) );
-    fields.push_back( encodeUInt(0) );
-
+    fields.push_back(encodeUInt(tx.chainId));
+    fields.push_back(encodeUInt(tx.nonce));
+    fields.push_back(encodeUInt(tx.maxPriorityFeePerGas));
+    fields.push_back(encodeUInt(tx.maxFeePerGas));
+    fields.push_back(encodeUInt(tx.gasLimit));
+    fields.push_back(encodeBytes(tx.to));
+    fields.push_back(encodeBytes(tx.value));
+    fields.push_back(encodeBytes(tx.data));
+    fields.push_back(encodeUInt(0));
     return encodeList(fields);
 }
 
 std::vector<uint8_t> signEvmTransaction(
-    EvmTx &tx,
-    const std::array<uint8_t,32> &privkey
-) {
+    EvmTx                        &tx,
+    const std::array<uint8_t,32> &privkey)
+{
     auto unsignedRlp = buildUnsignedRlp(tx);
 
     auto hashVec = crypto::Keccak256(unsignedRlp);
-    std::array<uint8_t,32> hash{};
+    std::array<uint8_t, 32> hash{};
     std::copy(hashVec.begin(), hashVec.end(), hash.begin());
 
-    auto [rArr, sArr, v] = crypto::signHash(hash, privkey);
+    // signHashWithKey takes privkey directly -- no file path needed here
+    auto [rArr, sArr, v] = signHashWithKey(hash, privkey);
 
     tx.r.assign(rArr.begin(), rArr.end());
     tx.s.assign(sArr.begin(), sArr.end());
@@ -44,18 +44,17 @@ std::vector<uint8_t> signEvmTransaction(
 
     using namespace rlp;
     std::vector<std::vector<uint8_t>> finalFields;
-    finalFields.push_back( encodeUInt(tx.chainId) );
-    finalFields.push_back( encodeUInt(tx.nonce) );
-    finalFields.push_back( encodeUInt(tx.maxPriorityFeePerGas) );
-    finalFields.push_back( encodeUInt(tx.maxFeePerGas) );
-    finalFields.push_back( encodeUInt(tx.gasLimit) );
-    finalFields.push_back( encodeBytes(tx.to) );
-    finalFields.push_back( encodeBytes(tx.value) );
-    finalFields.push_back( encodeBytes(tx.data) );
-    finalFields.push_back( encodeUInt(tx.v) );
-    finalFields.push_back( encodeBytes(tx.r) );
-    finalFields.push_back( encodeBytes(tx.s) );
-
+    finalFields.push_back(encodeUInt(tx.chainId));
+    finalFields.push_back(encodeUInt(tx.nonce));
+    finalFields.push_back(encodeUInt(tx.maxPriorityFeePerGas));
+    finalFields.push_back(encodeUInt(tx.maxFeePerGas));
+    finalFields.push_back(encodeUInt(tx.gasLimit));
+    finalFields.push_back(encodeBytes(tx.to));
+    finalFields.push_back(encodeBytes(tx.value));
+    finalFields.push_back(encodeBytes(tx.data));
+    finalFields.push_back(encodeUInt(tx.v));
+    finalFields.push_back(encodeBytes(tx.r));
+    finalFields.push_back(encodeBytes(tx.s));
     return encodeList(finalFields);
 }
 
