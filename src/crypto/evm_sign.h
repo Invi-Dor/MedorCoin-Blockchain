240340c846eea9f364160e2cf193c6395f9dd77c
food101ideas <food101.ideas@gmail.com>
2026-03-21 11:20:25 +0800

    Update evm_sign.h
    Signed-off-by: food101ideas <food101.ideas@gmail.com>

--- src/crypto/evm_sign.h
+++ src/crypto/evm_sign.h
@@ -1,31 +1,27 @@
-#ifndef CRYPTO_EVM_SIGN_H
-#define CRYPTO_EVM_SIGN_H
+#pragma once
 
-#include <vector>
 #include <array>
 #include <cstdint>
+#include <vector>
 
 namespace crypto {
 
 struct EvmTx {
-    uint64_t chainId;
-    uint64_t nonce;
-    uint64_t maxPriorityFeePerGas;
-    uint64_t maxFeePerGas;
-    uint64_t gasLimit;
+    uint64_t             chainId;
+    uint64_t             nonce;
+    uint64_t             maxPriorityFeePerGas;
+    uint64_t             maxFeePerGas;
+    uint64_t             gasLimit;
     std::vector<uint8_t> to;
     std::vector<uint8_t> value;
     std::vector<uint8_t> data;
     std::vector<uint8_t> r;
     std::vector<uint8_t> s;
-    uint8_t v;
+    uint8_t              v;
 };
 
 std::vector<uint8_t> signEvmTransaction(
-    EvmTx &tx,
-    const std::array<uint8_t,32> &privkey
-);
+    EvmTx                        &tx,
+    const std::array<uint8_t,32> &privkey);
 
 } // namespace crypto
-
-#endif // CRYPTO_EVM_SIGN_H
