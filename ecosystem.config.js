38f04fda71b11854db03e81d705582fb74ed1c82
stonash443 <julaventerprise@gmail.com>
2026-04-07 05:13:37 +0800

    Update ecosystem.config.js
    Signed-off-by: stonash443 <julaventerprise@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,6 @@ module.exports = {
     { name: "redis-6379", script: "redis-server", args: "--port 6379 --cluster-enabled yes --daemonize no" },
     { name: "redis-6380", script: "redis-server", args: "--port 6380 --cluster-enabled yes --daemonize no" },
     { name: "redis-6381", script: "redis-server", args: "--port 6381 --cluster-enabled yes --daemonize no" },
-    { name: "medor-engine", script: "npm start", delay: 5000 }
+    { name: "medor-engine", script: "npm start", delay: 5001 }
   ]
 };
