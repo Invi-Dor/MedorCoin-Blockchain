d9a13fbe68e384e684e76e2113940c12d8f8927f
stonash443 <julaventerprise@gmail.com>
2026-04-07 13:24:40 +0800

    Update MedorToken.sol
    Signed-off-by: stonash443 <julaventerprise@gmail.com>

--- contracts/MedorToken.sol
+++ contracts/MedorToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+// In v5.x, these moved to the 'utils' folder:
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
