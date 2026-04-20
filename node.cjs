7806110b50bbb4fe5cf2d500311449e8c38644c9
food101ideas <food101.ideas@gmail.com>
2026-04-21 06:07:44 +0800

    Update node.cjs
    Signed-off-by: food101ideas <food101.ideas@gmail.com>

--- node.cjs
+++ node.cjs
@@ -53,7 +53,7 @@ app.get('/status', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 MEDORCOIN GATEWAY LIVE ON PORT ${PORT}`);
 });
