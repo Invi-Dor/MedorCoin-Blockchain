0429268727802fd0631c0ea6ff09709ef75956cc
Ajvlogger <ajvlogger4@gmail.com>
2026-05-05 09:10:19 +0800

    Add Medorcoin Mainnet configuration
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

---
+++ chainid-8888.js
@@ -0,0 +1,24 @@
+{
+  "name": "Medorcoin Mainnet",
+  "chain": "MEDOR",
+  "rpc": [
+    "http://172.235.50.31:3000"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Medorcoin",
+    "symbol": "MEDOR",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://medorcoin.org",
+  "shortName": "medor",
+  "chainId": 8888,
+  "networkId": 8888,
+  "icon": "medorcoin",
+  "explorers": [{
+    "name": "MedorExplorer",
+    "url": "https://medorcoin.org",
+    "standard": "EIP3091"
+  }]
+}
