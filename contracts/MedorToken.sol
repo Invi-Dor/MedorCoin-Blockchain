dae0360c8d84ae4c113f73afbe344d311a2fad1e
Ajvlogger <ajvlogger4@gmail.com>
2026-05-13 06:59:25 +0800

    Update MedorToken.sol
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- contracts/MedorToken.sol
+++ contracts/MedorToken.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// Corrected path for OpenZeppelin v5.x
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/utils/ReentrancyGuard.sol";
 
 /**
  * @title MedorToken (MEDOR)
